023ac67273efec34d1204cbc6958c8c5a849410e
Carlos Tanseco <cmtanseco@gmail.com>
2019-03-20 17:25:34 -0500

    Added validations to the Product model.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :designer
   belongs_to :category
+
+  validates :name, :price, :stock, :size, :description, presence: true
+  validates :stock, :price, numericality: { only_integer: true }
+  validates :name, uniqueness: true
 end
