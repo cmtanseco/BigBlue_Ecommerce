7f1fbf96f28817a55ca417afb72d971df8c7e765
Carlos Tanseco <cmtanseco@gmail.com>
2019-03-21 22:51:14 -0500

    Generated Orders model.

---
+++ db/migrate/20190322035037_create_orders.rb
@@ -0,0 +1,13 @@
+class CreateOrders < ActiveRecord::Migration[5.2]
+  def change
+    create_table :orders do |t|
+      t.datetime :orderdate
+      t.integer :quantity
+      t.string :status
+      t.references :customer, foreign_key: true
+      t.references :product, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
