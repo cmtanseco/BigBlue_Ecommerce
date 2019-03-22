9f9c30ffecbdd3981b4245c5134fde9b5bd468cf
Carlos Tanseco <cmtanseco@gmail.com>
2019-03-21 23:35:39 -0500

    Added missing column.

--- db/migrate/20190322035037_create_orders.rb
+++ db/migrate/20190322035037_create_orders.rb
@@ -4,6 +4,7 @@ class CreateOrders < ActiveRecord::Migration[5.2]
       t.datetime :orderdate
       t.integer :quantity
       t.string :status
+      t.decimal :total
       t.references :customer, foreign_key: true
       t.references :product, foreign_key: true
 
