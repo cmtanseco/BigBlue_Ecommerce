ff8330389bd85c910be3c72d8ccf91e0a73c1f18
Carlos Tanseco <cmtanseco@gmail.com>
2019-03-22 01:23:05 -0500

    Added dashboard editability.

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,6 @@ class Product < ApplicationRecord
   belongs_to :category
 
   validates :name, :price, :stock, :size, :description, presence: true
-  validates :stock, :price, numericality: { only_integer: true }
+  validates :stock, numericality: { only_integer: true }
   validates :name, uniqueness: true
 end
