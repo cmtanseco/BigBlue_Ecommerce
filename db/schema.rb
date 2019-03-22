fbe4695db6df290b6d1329b1d96a03bc5d8e8fd4
Carlos Tanseco <cmtanseco@gmail.com>
2019-03-21 23:45:30 -0500

    Scaffolded Pages.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_22_035037) do
+ActiveRecord::Schema.define(version: 2019_03_22_044051) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -76,6 +76,14 @@ ActiveRecord::Schema.define(version: 2019_03_22_035037) do
     t.datetime "updated_at", null: false
     t.index ["customer_id"], name: "index_orders_on_customer_id"
     t.index ["product_id"], name: "index_orders_on_product_id"
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "permalink"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
