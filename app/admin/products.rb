2113fc75be24e006619ea6ed8f41ebd6fc3d28d0
Carlos Tanseco <cmtanseco@gmail.com>
2019-03-22 14:12:43 -0500

    Added images fuctionality for products.

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -11,5 +11,5 @@ ActiveAdmin.register Product do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-  permit_params :name, :price, :size, :stock,:description, :designer_id, :category_id
+  permit_params :name, :price, :size, :stock,:description, :designer_id, :category_id, :image
 end
