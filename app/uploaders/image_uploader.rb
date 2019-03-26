b8b830c10554b5a2d746e38942812b2885ab0f8f
Carlos Tanseco <cmtanseco@gmail.com>
2019-03-26 10:15:42 -0500

    Changed show product image height.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -34,7 +34,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   version :show do
-    process resize_to_fit: [nil, 820]
+    process resize_to_fit: [nil, 735]
   end
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
